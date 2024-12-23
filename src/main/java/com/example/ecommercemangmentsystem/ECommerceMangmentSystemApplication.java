a47eb3bb6aed49e6dcee2d9480576e5b4a412e70
Mamatha Pudukutapu <mammucodes@gmail.com>
2024-12-23 15:41:07 -0600

    intital commit of model package  classes

---
+++ src/main/java/com/example/ecommercemangmentsystem/ECommerceMangmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemangmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMangmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceMangmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ecommercemangmentsystem/model/CartItem.java
@@ -0,0 +1,34 @@
+package com.example.ecommercemangmentsystem.model;
+
+
+
+public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "product=" + product +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
+
+
